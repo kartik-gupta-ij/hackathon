df33948a0b285c87497d638fd496907399430bc6
Kartik Gupta <kartikgupta7267@gmail.com>
2023-05-08 10:28:41 +0530

    4th

--- core/core/urls.py
+++ core/core/urls.py
@@ -21,6 +21,7 @@ from home.views import *
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -4,7 +4,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/index.html')
+
+    peoples=[
+        {'name':'John', 'age': 23}, 
+        {'name':'Jhne', 'age': 3},
+        {'name':'Jgne', 'age': 2433},
+        {'name':'Jne', 'age': 6},
+        {'name':'Jane', 'age': 23},
+    ]
+
+    return render(request, 'home/index.html' ,context={'peoples':peoples, 'page':'home'})
 
 def about(request):
-    return HttpResponse('<h1>about</h1>')
+    context={'page':'about'}
+    return render(request, 'home/about.html', context)
+
+def contact(request):
+    context={'page':'contact'}
+    return render(request, 'home/contact.html', context)
